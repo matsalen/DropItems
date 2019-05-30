cb7f805813370a4006c542ac44b2dd75e1cf6a75
matsalen <k.kurosaki@hotmail.com>
2019-05-30 17:28:53 -0300

    Código para gerar itens aleatórios.

---
+++ DropItem/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] itens;
+    [SerializeField] private GameObject referencia;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(itens[Random.Range(0, itens.Length)], referencia.transform.position, Quaternion.identity);
+        }
+    }
+}
